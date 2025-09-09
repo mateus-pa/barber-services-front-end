f91d8e6cc5bba52554b5f77b1c5e6a1c42616dcf
MATEUS PINHEIRO CONDE DE ALMEIDA <025.026817@universidadesenaicimatec.edu.br>
2025-09-09 15:47:48 -0300

    feat: cria component footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [ TooltipModule, CommonModule, Header],
+  imports: [TooltipModule, CommonModule, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('barberServices');
