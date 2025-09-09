44cd24b9c637301af895d13694dcb1d17b0f890e
mateus-pa <mateuspinheiro1410@hotmail.com>
2025-09-09 00:04:16 -0300

    feat: configura aplicação e instala ngx bootsrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TooltipModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
