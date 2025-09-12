8c3b5715eb8bc115151a06a4c4e6dd51263b677d
MATEUS PINHEIRO CONDE DE ALMEIDA <025.026817@universidadesenaicimatec.edu.br>
2025-09-12 16:18:47 -0300

    fix: adiciona router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from './footer/footer';
 import { Funcoes } from './funcoes/funcoes';
 import { Header } from './header/header';
 import { Sobre } from './sobre/sobre';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [TooltipModule, CommonModule, Header, Footer, Sobre, Funcoes],
+  imports: [TooltipModule, CommonModule, Header, Footer, Sobre, Funcoes, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
