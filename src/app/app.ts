6dba6ba28e40f5b15785142340bfef5d6bb02dbe
mateus-pa <mateuspinheiro1410@hotmail.com>
2025-09-12 22:47:18 -0300

    feat: adiciona banner e menu de navegação sticky

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { Banner } from './banner/banner';
 import { Footer } from './footer/footer';
 import { Funcoes } from './funcoes/funcoes';
 import { Header } from './header/header';
 import { Sobre } from './sobre/sobre';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [TooltipModule, CommonModule, Header, Footer, Sobre, Funcoes, RouterOutlet],
+  imports: [TooltipModule, CommonModule, Header, Footer, Sobre, Funcoes, RouterOutlet, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,9 @@
-import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass, ViewportScroller } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
@@ -13,5 +13,20 @@ export class Header {
 
   scrollToElement(fragment: string): void {
       this.scroller.scrollToAnchor(fragment);
+  }
+
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', [])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    
+    const stickyThreshold = 20; 
+
+    if (scrollPosition >= stickyThreshold) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
   }
 }
