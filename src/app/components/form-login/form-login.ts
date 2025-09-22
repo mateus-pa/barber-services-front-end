b71cebfafffd0000ef6b3f5d461f05271ffe3af2
MATEUS PINHEIRO CONDE DE ALMEIDA <025.026817@universidadesenaicimatec.edu.br>
2025-09-22 16:58:02 -0300

    refactor: altera formulário de login

--- src/app/components/form-login/form-login.ts
+++ src/app/components/form-login/form-login.ts
@@ -1,17 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-form-login',
-  imports: [FormsModule, CommonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './form-login.html',
   styleUrl: './form-login.css',
 })
 export class FormLogin {
-  constructor() {}
-
   ngOnInit(): void {}
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  errorMessage = signal('');
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('Você precisa digitar um email');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Não é um email válido');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 
   onSubmit(form: NgForm): void {
     if (form.invalid) {
