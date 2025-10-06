72f8e136c612e5623e7bb457d55dc0f783f252e0
mateus-pa <mateuspinheiro1410@hotmail.com>
2025-10-06 14:05:52 -0300

    feat: aprimora erros ao efetuar cadastro

--- src/app/components/form-cadastro/form-cadastro.ts
+++ src/app/components/form-cadastro/form-cadastro.ts
@@ -22,19 +22,17 @@ export class FormCadastro {
   ngOnInit(): void {}
   cadastroFormGroup: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     aceiteTermos: new FormControl(false, [Validators.requiredTrue]),
   });
 
   email = this.cadastroFormGroup.get('email') as FormControl;
-  senha = this.cadastroFormGroup.get('senha') as FormControl;
+  password = this.cadastroFormGroup.get('password') as FormControl;
   nome = this.cadastroFormGroup.get('nome') as FormControl;
   aceiteTermos = this.cadastroFormGroup.get('aceiteTermos') as FormControl;
 
-  toggleButton(): void {
-    // console.log('Termos de aceite mudou para:' + this.aceiteTermos.value);
-  }
+  toggleButton(): void {}
 
   protected onInput(event: Event) {
     this.value.set((event.target as HTMLInputElement).value);
@@ -43,18 +41,64 @@ export class FormCadastro {
   constructor() {
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
+      .subscribe(() => this.updateErrorMessageEmail());
+
+    merge(this.password.statusChanges, this.password.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessagePassword());
+
+    merge(this.aceiteTermos.statusChanges, this.aceiteTermos.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessageAceiteTermos());
+
+    merge(this.nome.statusChanges, this.nome.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessageNome());
   }
 
-  errorMessage = signal('');
-  updateErrorMessage() {
+  errorMessageNome = signal('');
+  updateErrorMessageNome() {
+    const nomeControl = this.cadastroFormGroup.get('nome');
+    if (nomeControl?.hasError('required')) {
+      this.errorMessageNome.set('Você precisa digitar um nome');
+    } else if (nomeControl?.hasError('minlength')) {
+      this.errorMessageNome.set('O nome precisa ter pelo menos 3 caracteres');
+    } else {
+      this.errorMessageNome.set('');
+    }
+  }
+
+  errorMessageEmail = signal('');
+  updateErrorMessageEmail() {
     const emailControl = this.cadastroFormGroup.get('email');
     if (emailControl?.hasError('required')) {
-      this.errorMessage.set('Você precisa digitar um email');
+      this.errorMessageEmail.set('Você precisa digitar um email');
     } else if (emailControl?.hasError('email')) {
-      this.errorMessage.set('Não é um email válido');
+      this.errorMessageEmail.set('Não é um email válido');
     } else {
-      this.errorMessage.set('');
+      this.errorMessageEmail.set('');
+    }
+  }
+
+  errorMessagePassword = signal('');
+  updateErrorMessagePassword() {
+    const passwordControl = this.cadastroFormGroup.get('password');
+    if (passwordControl?.hasError('required')) {
+      this.errorMessagePassword.set(' Vocé precisa digitar uma senha');
+    } else if (passwordControl?.hasError('minlength')) {
+      this.errorMessagePassword.set('A senha precisa ter pelo menos 6 caracteres');
+    } else {
+      this.errorMessagePassword.set('');
+    }
+  }
+
+  errorMessageAceiteTermos = signal('');
+  updateErrorMessageAceiteTermos() {
+    const aceiteTermosControl = this.cadastroFormGroup.get('aceiteTermos');
+    if (aceiteTermosControl?.hasError('required')) {
+      this.errorMessageAceiteTermos.set('Você precisa aceitar os termos de serviço');
+    } else {
+      this.errorMessageAceiteTermos.set('');
     }
   }
 
