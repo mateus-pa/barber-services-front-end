573d0762cfb4d347ec43ee67364ff840cd8d8480
mateus-pa <mateuspinheiro1410@hotmail.com>
2025-10-15 20:41:02 -0300

    feat: elabora estilização modal adicionar cliente

--- src/app/components/modal-adicionar-cliente/modal-adicionar-cliente.ts
+++ src/app/components/modal-adicionar-cliente/modal-adicionar-cliente.ts
@@ -5,6 +5,11 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { firstValueFrom } from 'rxjs';
 import { QueueService } from '../../services/queue';
 
+interface ModalData {
+  expertId: string;
+  CreatedAt: string;
+}
+
 @Component({
   selector: 'app-modal-adicionar-cliente',
   standalone: true,
@@ -14,18 +19,40 @@ import { QueueService } from '../../services/queue';
 })
 export class ModalAdicionarCliente {
   private dialogRef = inject(MatDialogRef<ModalAdicionarCliente>);
-  private data = inject(MAT_DIALOG_DATA);
+  private data: ModalData = inject(MAT_DIALOG_DATA);
   private queueService = inject(QueueService);
 
   cliente = {
     name: '',
     service: '',
-    appointmentTime: '',
+    time: '',
   };
+
+  private readonly BRAZIL_TIMEZONE_OFFSET = 3 * 60;
 
   async adicionar() {
     try {
-      await firstValueFrom(this.queueService.adicionarCliente(this.data.expertId, this.cliente));
+      const queueDate = new Date(this.data.CreatedAt);
+
+      queueDate.setHours(0, 0, 0, 0);
+
+      const [hours, minutes] = this.cliente.time.split(':').map(Number);
+
+      queueDate.setHours(hours, minutes, 0, 0);
+      const localOffset = queueDate.getTimezoneOffset();
+
+      const targetOffset = localOffset - this.BRAZIL_TIMEZONE_OFFSET;
+
+      const targetDate = new Date(queueDate.getTime() - targetOffset * 60 * 1000);
+
+      const appointmentTimeISO = targetDate.toISOString();
+      const clientPayload = {
+        name: this.cliente.name,
+        service: this.cliente.service,
+        appointmentTime: appointmentTimeISO,
+      };
+
+      await firstValueFrom(this.queueService.adicionarCliente(this.data.expertId, clientPayload));
       this.dialogRef.close(true);
     } catch (err) {
       console.error('Erro ao adicionar cliente:', err);
